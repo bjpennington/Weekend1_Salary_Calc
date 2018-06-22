d87c7a1239770c747a80787e8807cbbb83776c0f
BJ Pennington <bjpennington112@gmail.com>
2018-06-22 16:11:01 -0500

    built Employee class constructor

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,21 @@
+const employees = [];
+
+class Employee {
+    constructor(firstName, lastName, idNum, title, salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNum = idNum;
+        this.title = title;
+        this.salary = salary;
+    }
+}
+
+function newEmployee(firstName, lastName, idNum, title, salary) {
+    let employee = new Employee(firstName, lastName, idNum, title, salary);
+    employees.push(employee);
+}
+
+
 $(readyNow);
 
 function readyNow() {
