690f6be9b2b7b1496903e73b600ec588f579c7c8
BJ Pennington <bjpennington112@gmail.com>
2018-06-22 18:01:27 -0500

    added checkCosts function

--- scripts/client.js
+++ scripts/client.js
@@ -41,23 +41,33 @@ function handleSubmit() {
     let idNum = $('#idNumInput').val();
     let title = $('#titleInput').val();
     let salary = $('#salaryInput').val();
-    
+
     // use input values to create new objects in the array
     newEmployee(firstName, lastName, idNum, title, salary);
-    
+
     // append new table row with input data
     employeesToTable();
 
     // update total expense based on additonal salary
     updateExpense(salary);
-    
+
+    checkCosts();
+
+    $('#fnInput').val('');
+    $('#lnInput').val('');
+    $('#idNumInput').val('');
+    $('#titleInput').val('');
+    $('#salaryInput').val('');
+
+
+
 }
 
 function employeesToTable() {
 
     // clear current table data
     $('#salaryTable').empty();
-    
+
     // add table data built from employees array
     for (employee of employees) {
         addTableRow(employee.firstName, employee.lastName, employee.idNum, employee.title, employee.salary);
@@ -67,7 +77,7 @@ function employeesToTable() {
 function addTableRow(firstName, lastName, idNum, title, salary) {
     // use passed in parameters to create a table row
     let $row = $('<tr></tr>');
-    
+
     $row.append(`<td>${firstName}</td>`);
     $row.append(`<td>${lastName}</td>`);
     $row.append(`<td>${idNum}</td>`);
@@ -80,9 +90,15 @@ function addTableRow(firstName, lastName, idNum, title, salary) {
 function updateExpense(salary) {
 
     // calculate monthly expense for passed in salary and add to total expense
-    totalExpense += salary/12;
+    totalExpense += salary / 12;
     //return totalExpense;
 
     // append updated expenses to the DOM
     $('#monthlyTotal').text('Total Monthly: ' + totalExpense);
+}
+
+function checkCosts() {
+    if(totalExpense > 20000) {
+        $('#monthlyTotal').addClass('redBack');
+    }
 }
